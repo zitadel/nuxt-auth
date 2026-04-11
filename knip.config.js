0851741522d6699473a070855d593e71d0c70dd8
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-04-11 09:55:14 +0700

    chore: add commitlint configuration

--- knip.config.js
+++ knip.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   ignoreDependencies: [
+    '@commitlint/config-conventional',
     '@semantic-release/.*?',
     'vitest-environment-nuxt',
     'ofetch',
   ],
   ignoreBinaries: ['playwright'],
   entry: ['src/module.ts', 'src/runtime/**/*.ts'],
-  ignore: ['playground-authjs/**'],
+  ignore: ['commitlint.config.js', 'playground-authjs/**'],
 }
